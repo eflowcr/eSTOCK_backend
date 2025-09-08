c610419ef8b83ff9eb1e6578ea1db7d4bd7d7c49
Ehitel I Rodriguez <ehitelrj@gmail.com>
2025-09-08 14:25:29 -0600

    refactor: update CreateInventory and CreateLotRequest structures for consistency and improved date handling

--- models/requests/create_inventory.go
+++ models/requests/create_inventory.go
@@ -3,14 +3,12 @@ package requests
 import "github.com/eflowcr/eSTOCK_backend/models/database"
 
 type CreateInventory struct {
-	SKU           string            `json:"sku" validate:"required"`
-	Name          string            `json:"name" validate:"required"`
-	Description   *string           `json:"description"`
-	Location      string            `json:"location" validate:"required"`
-	Quantity      float64           `json:"quantity" validate:"required"`
-	UnitPrice     *float64          `json:"unitPrice" validate:"required"`
-	Lots          []database.Lot    `json:"lots,omitempty"`
-	Serials       []database.Serial `json:"serials,omitempty"`
-	TrackByLot    bool              `json:"trackByLot"`
-	TrackBySerial bool              `json:"trackBySerial"`
+	SKU         string             `json:"sku" validate:"required"`
+	Name        string             `json:"name" validate:"required"`
+	Description *string            `json:"description"`
+	Location    string             `json:"location" validate:"required"`
+	Quantity    float64            `json:"quantity" validate:"required"`
+	UnitPrice   *float64           `json:"unitPrice" validate:"required"`
+	Lots        []CreateLotRequest `json:"lots,omitempty"`
+	Serials     []database.Serial  `json:"serials,omitempty"`
 }
